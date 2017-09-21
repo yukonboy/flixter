2c02bb5238840cd0a02fb2fa6260a5f9547114d8
yukonboy <yukonboy@telus.net>
2017-09-21 17:43:31 +0000

    Added and formatted a footer with Flexbox

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -44,8 +44,8 @@ SimpleForm.setup do |config|
     b.optional :readonly
 
     ## Inputs
-    b.use :label_input, :hint, wrap_with: { tag: :span, class: :hint }
-    b.use :hint 
+    b.use :label_input
+    b.use :hint,  wrap_with: { tag: :span, class: :hint }
     b.use :error, wrap_with: { tag: :span, class: :error }
 
     ## full_messages_for
