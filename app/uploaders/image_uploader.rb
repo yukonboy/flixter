6cb4d44be0dd67ef6cc28f6f5f64ad742f97d6bb
yukonboy <yukonboy@telus.net>
2017-09-26 20:35:07 +0000

    Installed (previously, but again now) Imagemagick and miniMagick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [70, 100]
+    process resize_to_fit: [800, 350]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
